ff398ba7513ce3c7de1fcb1b96d24dffd53f92f3
Hanard Valentin <valentin.hanard@laposte.net>
2018-01-31 17:20:24 +0100

    Creation Entity News et test sur page News

--- src/HV/NewsBundle/Controller/NewsController.php
+++ src/HV/NewsBundle/Controller/NewsController.php
@@ -2,19 +2,35 @@
 
 namespace HV\NewsBundle\Controller;
 
+use HV\NewsBundle\Entity\News;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 
 
 class NewsController extends Controller
 {
     public function indexAction()
     {
-      return $this->render('@HVNews/News/index.html.twig');
+      $repository = $this->getDoctrine()
+        ->getManager()
+        ->getRepository('HVNewsBundle:News')
+      ;
+      $listNews = $repository->findAll();
+      return $this->render('@HVNews/News/index.html.twig', array('listNews' => $listNews));
     }
 
-    public function viewCurrentEventsAction($url)
+    public function viewCurrentEventsAction($id)
     {
-      return $this->render('@HVNews/News/viewCurrentEvents.html.twig', array('id' => $url));
+      // On récupère le repository
+      $repository = $this->getDoctrine()
+        ->getManager()
+        ->getRepository('HVNewsBundle:News')
+      ;
+      $news = $repository->find($id);
+      if (null === $news) {
+        throw new NotFoundHttpException("La news d'id ".$id." n'existe pas.");
+      }
+      return $this->render('@HVNews/News/viewCurrentEvents.html.twig', array('news' => $news));
     }
 }
---
+++ src/HV/NewsBundle/Entity/News.php
@@ -0,0 +1,193 @@
+<?php
+
+namespace HV\NewsBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * News
+ *
+ * @ORM\Table(name="news")
+ * @ORM\Entity(repositoryClass="HV\NewsBundle\Repository\NewsRepository")
+ */
+class News
+{
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="title", type="string", length=255)
+     */
+    private $title;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="author", type="string", length=255)
+     */
+    private $author;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="content", type="text")
+     */
+    private $content;
+
+    /**
+     * @var \DateTime
+     *
+     * @ORM\Column(name="date_news", type="datetime")
+     */
+    private $dateNews;
+
+    /**
+     * @ORM\Column(name="in_carousel", type="boolean")
+     */
+    private $inCarousel = false;
+
+
+    public function __construct()
+    {
+      // Par défaut, la date de l'annonce est la date d'aujourd'hui
+      $this->dateNews = new \Datetime();
+    }
+
+    /**
+     * Get id
+     *
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set title
+     *
+     * @param string $title
+     *
+     * @return News
+     */
+    public function setTitle($title)
+    {
+        $this->title = $title;
+
+        return $this;
+    }
+
+    /**
+     * Get title
+     *
+     * @return string
+     */
+    public function getTitle()
+    {
+        return $this->title;
+    }
+
+    /**
+     * Set author
+     *
+     * @param string $author
+     *
+     * @return News
+     */
+    public function setAuthor($author)
+    {
+        $this->author = $author;
+
+        return $this;
+    }
+
+    /**
+     * Get author
+     *
+     * @return string
+     */
+    public function getAuthor()
+    {
+        return $this->author;
+    }
+
+    /**
+     * Set content
+     *
+     * @param string $content
+     *
+     * @return News
+     */
+    public function setContent($content)
+    {
+        $this->content = $content;
+
+        return $this;
+    }
+
+    /**
+     * Get content
+     *
+     * @return string
+     */
+    public function getContent()
+    {
+        return $this->content;
+    }
+
+    /**
+     * Set dateNews
+     *
+     * @param \DateTime $dateNews
+     *
+     * @return News
+     */
+    public function setDateNews($dateNews)
+    {
+        $this->dateNews = $dateNews;
+
+        return $this;
+    }
+
+    /**
+     * Get dateNews
+     *
+     * @return \DateTime
+     */
+    public function getDateNews()
+    {
+        return $this->dateNews;
+    }
+
+    /**
+     * Set inCarousel
+     *
+     * @param boolean $inCarousel
+     *
+     * @return News
+     */
+    public function setInCarousel($inCarousel)
+    {
+        $this->inCarousel = $inCarousel;
+
+        return $this;
+    }
+
+    /**
+     * Get inCarousel
+     *
+     * @return boolean
+     */
+    public function getInCarousel()
+    {
+        return $this->inCarousel;
+    }
+}
