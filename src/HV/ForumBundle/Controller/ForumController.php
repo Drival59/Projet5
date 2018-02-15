0ddc5cdefa6cb6ca03fbb02d8699f418df65ed97
Hanard Valentin <valentin.hanard@laposte.net>
2018-02-15 17:09:27 +0100

    Creation squelette forum et entités categorie et section forum

--- src/HV/ForumBundle/Controller/ForumController.php
+++ src/HV/ForumBundle/Controller/ForumController.php
@@ -2,12 +2,21 @@
 
 namespace HV\ForumBundle\Controller;
 
+use HV\ForumBundle\Entity\ForumCategory;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Session\Session;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
 class ForumController extends Controller
 {
     public function indexAction()
     {
-        return $this->render('@HVForum/Forum/index.html.twig');
+      $em = $this->getDoctrine()->getManager();
+      $listCategory = $em->getRepository('HVForumBundle:ForumCategory')->findAll();
+      $listSection = $em->getRepository('HVForumBundle:ForumSection')->findAll();
+      return $this->render('@HVForum/Forum/index.html.twig', array(
+        'listCategory' => $listCategory,
+        'listSection' => $listSection,
+      ));
     }
 }
---
+++ src/HV/ForumBundle/Entity/ForumSection.php
@@ -0,0 +1,95 @@
+<?php
+
+namespace HV\ForumBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * ForumSection
+ *
+ * @ORM\Table(name="forum_section")
+ * @ORM\Entity(repositoryClass="HV\ForumBundle\Repository\ForumSectionRepository")
+ */
+class ForumSection
+{
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @ORM\ManyToOne(targetEntity="HV\ForumBundle\Entity\ForumCategory")
+     * @ORM\JoinColumn(nullable=false)
+     */
+    private $forumCategory;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="name", type="string", length=255)
+     */
+    private $name;
+
+
+    /**
+     * Get id
+     *
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set name
+     *
+     * @param string $name
+     *
+     * @return ForumSection
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    /**
+     * Get name
+     *
+     * @return string
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * Set forumCategory
+     *
+     * @param \HV\ForumBundle\Entity\ForumCategory $forumCategory
+     *
+     * @return ForumSection
+     */
+    public function setForumCategory(\HV\ForumBundle\Entity\ForumCategory $forumCategory)
+    {
+        $this->forumCategory = $forumCategory;
+
+        return $this;
+    }
+
+    /**
+     * Get forumCategory
+     *
+     * @return \HV\ForumBundle\Entity\ForumCategory
+     */
+    public function getForumCategory()
+    {
+        return $this->forumCategory;
+    }
+}
