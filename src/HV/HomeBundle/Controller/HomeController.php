37818aea99d0d16cd5413e216f945661604118da
Hanard Valentin <valentin.hanard@laposte.net>
2018-02-20 14:15:39 +0100

    Ajout formulaire contact sur page accueil et reponsive fini sur la page Home

--- src/HV/HomeBundle/Controller/HomeController.php
+++ src/HV/HomeBundle/Controller/HomeController.php
@@ -2,9 +2,6 @@
 
 namespace HV\HomeBundle\Controller;
 
-use HV\NewsBundle\Entity\News;
-use HV\UsersBundle\Entity\Users;
-use HV\UsersBundle\Form\UsersType;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Session\Session;
@@ -19,13 +16,23 @@ class HomeController extends Controller
       $lastNews = $repository->getRepository('HVNewsBundle:News')->getLastNews(3);
 
       if ($request->isMethod('POST')) {
-        $connection = $repository->getRepository('HVUsersBundle:Users')->getConnection($_POST['email'], $_POST['password']);
-        if ($connection != false) {
-          $session = new Session();
-          $session->set('User', $connection);
-          return $this->redirectToRoute('hv_home_homepage');
+        if (isset($_POST['email']) AND isset($_POST['password'])) {
+          $connection = $repository->getRepository('HVUsersBundle:Users')->getConnection($_POST['email'], $_POST['password']);
+          if ($connection != false) {
+            $session = new Session();
+            $session->set('User', $connection);
+            return $this->redirectToRoute('hv_home_homepage');
+          } else {
+            $this->addFlash('danger', 'Identifiant ou mot de passe incorrects');
+            return $this->redirectToRoute('hv_home_homepage');
+          }
         } else {
-          $this->addFlash('danger', 'Identifiant ou mot de passe incorrects');
+          $message = (new \Swift_Message($_POST['formSubjectContact']))
+            ->setFrom($_POST['formEmailContact'])
+            ->setTo($this->getParameter('mailer_user'))
+            ->setBody('<strong>' . $_POST['formNameContact'] . '</strong> vous a écrit : <br /><br />' . $_POST['formMessageContact'], 'text/html');
+          $this->get('mailer')->send($message);
+          $this->addFlash('success', 'Merci ! Votre mail a bien été envoyé. Nous vous recontacterons au plus vite.');
           return $this->redirectToRoute('hv_home_homepage');
         }
       }
