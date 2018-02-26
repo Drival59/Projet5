d2ad5a4222fb4c9c7fa7c5297ac0c5b7072832b6
Hanard Valentin <valentin.hanard@laposte.net>
2018-02-26 15:54:37 +0100

    Finalisation page home, ajout favicon

--- src/HV/HomeBundle/Controller/HomeController.php
+++ src/HV/HomeBundle/Controller/HomeController.php
@@ -11,22 +11,23 @@ class HomeController extends Controller
 {
     public function indexAction(Request $request)
     {
-      $repository = $this->getDoctrine()->getManager();
-
-      $lastNews = $repository->getRepository('HVNewsBundle:News')->getLastNews(3);
+      $em = $this->getDoctrine()->getManager();
+      $lastNews = $em->getRepository('HVNewsBundle:News')->getLastNews(3);
 
       if ($request->isMethod('POST')) {
+
         if (isset($_POST['email']) AND isset($_POST['password'])) {
-          $connection = $repository->getRepository('HVUsersBundle:Users')->getConnection($_POST['email'], $_POST['password']);
+          $connection = $em->getRepository('HVUsersBundle:Users')->getConnection($_POST['email'], $_POST['password']);
           if ($connection != false) {
             $session = new Session();
             $session->set('User', $connection);
             return $this->redirectToRoute('hv_home_homepage');
-          } else {
-            $this->addFlash('danger', 'Identifiant ou mot de passe incorrects');
-            return $this->redirectToRoute('hv_home_homepage');
           }
+          $this->addFlash('danger', 'Identifiant ou mot de passe incorrects');
+          return $this->redirectToRoute('hv_home_homepage');
+
         } else {
+
           $message = (new \Swift_Message($_POST['formSubjectContact']))
             ->setFrom($_POST['formEmailContact'])
             ->setTo($this->getParameter('mailer_user'))
@@ -34,8 +35,10 @@ class HomeController extends Controller
           $this->get('mailer')->send($message);
           $this->addFlash('success', 'Merci ! Votre mail a bien été envoyé. Nous vous recontacterons au plus vite.');
           return $this->redirectToRoute('hv_home_homepage');
+          
         }
       }
+
       return $this->render('@HVHome/Home/index.html.twig', array(
         'lastNews' => $lastNews,
       ));
